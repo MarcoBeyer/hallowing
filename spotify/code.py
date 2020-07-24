82e510abbbd239d0a1cfdd309e792f7bbdb52453
Marco Beyer <MBeyer2@gmail.com>
2020-07-24 17:28:59 +0200

    initial commit

---
+++ spotify/code.py
@@ -0,0 +1,55 @@
+
+import board
+import busio
+import digitalio
+import time
+import touchio
+import displayio
+#import audioio
+
+beats = []
+
+led = digitalio.DigitalInOut(board.D13)
+led.direction = digitalio.Direction.OUTPUT
+
+uart = busio.UART(board.TX, board.RX, baudrate=9600, receiver_buffer_size=128)
+#wave_file = open("Rockface.wav", "rb")
+#wave = audioio.WaveFile(wave_file)
+#audio = audioio.AudioOut(board.A0)
+
+#splash = displayio.Group()
+#board.DISPLAY.show(splash)
+
+brightness_down = touchio.TouchIn(board.TOUCH2)
+brightness_up = touchio.TouchIn(board.TOUCH3)
+
+max_brightness = 0.5
+
+#with open('Yoshi.bmp', "rb") as f:
+#    try:
+#        odb = displayio.OnDiskBitmap(f)
+#    except ValueError:
+#        print("Image unsupported {}".format('Yoshi.bmp'))
+#    face = displayio.TileGrid(odb, pixel_shader=displayio.ColorConverter(), x=0, y=0)
+#    splash.append(face)
+#    # Wait for the image to load.
+#    board.DISPLAY.wait_for_frame()
+
+#audio.play(wave)
+
+while True:
+    data = uart.readline()
+
+    if data is not None:
+        data_string = bytes.decode(data, 'utf-8')
+        beats.extend(data_string.split());
+        print(data_string)
+    if brightness_up.value:
+        max_brightness += 0.1
+    elif brightness_down.value:
+        max_brightness -= 0.1
+    if max_brightness < 0:
+        max_brightness = 0
+    elif max_brightness >= 1:
+        max_brightness = 1.0
+    board.DISPLAY.brightness = max_brightness
